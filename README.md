9b96ace484ef94cc43fdb9ea3e4583fce7c7d447
Gullrye <wmwspecter@163.com>
2021-11-20 17:38:44 +0800

    use vue-amap

--- README.md
+++ README.md
@@ -1,30 +1,54 @@
 # OH 商城
 
-- 后端接口见 [newbee](https://github.com/newbee-ltd/newbee-mall-vue-app) 商城项目
+## 接口
 
-- npm
+[接口页面](http://47.99.134.126:28019/swagger-ui.html#/)
 
-  - 编译代码的时候 px 自动转化成 rem
-    - `$ npm install postcss postcss-pxtorem@5.1.1 --save-dev`，若装 6.0.0 版本会报错（报错信息：PostCSS plugin postcss-pxtorem requires PostCSS 8.）
-    - 大写的 PX 就不会被转化为 rem
-  - 验证码组件需要
-    - `npm install vue2-verify`
-  - 登录密码 md5 加密
-    - `npm install js-md5`
-  - ...
+详情见 [newbee](https://github.com/newbee-ltd/newbee-mall-vue-app) 商城项目。
 
-- 项目修改
+## npm
 
-  - 首页导航栏修改
-  - 分类页重写
-  - 订单页和地址页返回跳转修改
-  - 未登录跳转修改
-  - 购物车逻辑重写
-    - 添加商品时购物袋图标摇晃
-    - 能够直接点击「加入购物车」按钮来多次添加
-  - 配置 Vant 的地址栏
-  - Vuex 中设置 keyword，实现从 ProductDetail.vue 跳转到 ProductList.vue 时，保留搜索框关键字状态
-  - 地址管理链接 bug(this.cartItemIds 未赋值) 处理
+- 编译代码的时候 px 自动转化成 rem
+  - `$ npm install postcss postcss-pxtorem@5.1.1 --save-dev`，若装 6.0.0 版本会报错（报错信息：`PostCSS plugin postcss-pxtorem requires PostCSS 8.`）
+  - 大写的 `PX` 就不会被转化为 `rem`
+- 验证码组件需要
+  - `npm install vue2-verify`
+- 登录密码 md5 加密
+  - `npm install js-md5`
+- ...
+
+## 项目修改
+
+- 首页导航栏修改
+- 分类页重写
+- 订单页和地址页返回跳转修改
+- 未登录跳转修改
+- 购物车逻辑重写
+  - 添加商品时购物袋图标摇晃
+  - 能够直接点击「加入购物车」按钮来多次添加同一商品
+- 使用 Vant 的 Area 省市区选择及默认的省市区数据，实现地址管理
+- Vuex 中设置 `keyword`，实现从 `ProductDetail.vue` 跳转到 `ProductList.vue` 时，保留搜索框关键字状态
+- 地址管理链接 bug(`this.cartItemIds` 未赋值) 处理
+- About.vue 中添加地图
+
+  - `npm install -S vue-amap`
+  - 在 `.eslintrc.js` 中配置
+
+  ```js
+  globals: {
+    'AMap': false,
+  }
+  ```
+
+  - 实现拖动和点击时改变地址
+  - 官方文档网站案例代码显示不出来，可以查看 [issue](https://github.com/ElemeFE/vue-amap/issues/646) 中的回答，下载一份源码进行简单修改后，查看完整文档
+
+  ```html
+  // 将源码里 src/docs/index.html 下的
+  <script src="//unpkg.com/docsify/lib/docsify.min.js"></script>
+  // 修改为
+  <script src="//unpkg.com/docsify@4.11.6/lib/docsify.min.js"></script>
+  ```
 
 - 问题
 
@@ -32,7 +56,7 @@
 
   ```js
   // src/router/index.js 文件中
-  //获取原型对象上的push函数
+  //获取原型对象上的 push 函数
   const originalPush = VueRouter.prototype.push
   //修改原型对象中的push方法
   VueRouter.prototype.push = function push(location) {
@@ -47,6 +71,6 @@
   - 13606865391 111111
   - 13612345608 13612345678
 
-- 旧展示图
+- 展示图
 
 ![show](./img/GIF%202021-10-10%200-19-41.gif)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,104 @@
 <template>
   <div class="about">
     <s-header :name="'关于我们'"></s-header>
-    <h1>关于我们</h1>
-    <div class="intro">
-      1......<br />
-      2......<br />
-      3......<br />
-      4......<br />
-      5......<br />
-      6......<br />
-      7......
+    <div class="map-wrapper">
+      <h2 class="title map-title">联系地址</h2>
+      <!-- <p>position: [{{ lng }}, {{ lat }}]</p> -->
+      <p class="address-detail">地址: {{ address }}</p>
+      <div class="vue-map">
+        <el-amap
+          vid="amapDemo"
+          :center="center"
+          :zoom="zoom"
+          class="amap-demo"
+          :events="events"
+        >
+          <el-amap-marker
+            vid="component-marker"
+            :position="position"
+            :draggable="true"
+            :events="events"
+          ></el-amap-marker>
+        </el-amap>
+      </div>
+    </div>
+    <div class="msg-wrapper">
+      <h2 class="title">关于我们</h2>
+      <div class="intro">
+        1......<br />
+        2......<br />
+        3......<br />
+        4......<br />
+        5......<br />
+        6......<br />
+        7......
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import sHeader from '../components/SimpleHeader.vue'
+import VueAMap from 'vue-amap'
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+  key: 'cde7aa4cab0681d34ddb6916431125b2',
+  plugin: [
+    'AMap.Scale',
+    'AMap.OverView',
+    'AMap.ToolBar',
+    'AMap.MapType',
+    'Geocoder'
+  ],
+  v: '1.4.4'
+})
+
 export default {
   components: {
     sHeader
+  },
+  data() {
+    let self = this
+
+    return {
+      zoom: 12,
+      center: [116.391377, 23.04888],
+      address: '广东省汕头市百花山风景区',
+      lng: 116.391377,
+      lat: 23.04888,
+      // position: [116.391377, 23.04888],
+      // position: [this.lng, this.lat],
+      events: {
+        click(e) {
+          let { lng, lat } = e.lnglat
+          self.lng = lng
+          self.lat = lat
+
+          // 这里通过高德 SDK 完成。
+          var geocoder = new AMap.Geocoder({
+            radius: 1000,
+            extensions: 'all'
+          })
+          geocoder.getAddress([lng, lat], function (status, result) {
+            if (status === 'complete' && result.info === 'OK') {
+              if (result && result.regeocode) {
+                self.address = result.regeocode.formattedAddress
+                self.$nextTick()
+              }
+            }
+          })
+        },
+        dragend(e) {
+          self.events.click(e)
+        }
+      }
+    }
+  },
+  computed: {
+    position() {
+      return [this.lng, this.lat]
+    }
   }
 }
 </script>
@@ -27,7 +107,7 @@ export default {
 .about {
   color: #1baeae;
   text-align: center;
-  h1 {
+  .title {
     display: inline-block;
     width: 50%;
     margin-top: 50px;
@@ -36,7 +116,20 @@ export default {
     text-align: center;
   }
   .intro {
-    font-size: 30px;
+    font-size: 20px;
+    padding-bottom: 100px;
+  }
+  .vue-map {
+    width: 95%;
+    height: 400px;
+    margin: 0 auto;
+    .el-vue-amap-container {
+      border: 2px dashed #1baeae;
+    }
+  }
+  .address-detail {
+    text-align: center;
+    font-size: 15px;
   }
 }
 </style>
